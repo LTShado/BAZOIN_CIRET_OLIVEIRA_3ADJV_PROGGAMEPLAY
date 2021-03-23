4514d6121983674162a0f9bb0920136133afc12b
Rezay <rezaygame@gmail.com>
2021-03-23 14:41:40 +0100

    Revert "add shoot"
    This reverts commit 1b65dea6e8789a1116c22cda2e046228e2ae296b.

---
+++ Project/Assets/Scripts/AttackModules/AttackModule.cs
@@ -0,0 +1,7 @@
+public abstract class AttackModule 
+{
+    private void Awake()
+    {
+        
+    }
+}
--- Project/Assets/Scripts/GameManager.cs
+++ Project/Assets/Scripts/GameManager.cs
@@ -18,22 +18,12 @@ public class GameManager : MonoBehaviour
     {
         _singleton = this;
         new TAccessor<MovementModule>();
-        new TAccessor<ShootModule>();
     }
-    
-    List<PoolableObject> objectPool;
+
     // Start is called before the first frame update
     void Start()
     {
         UpdateList.Add(new MovementSystem());
-        UpdateList.Add(new ShootSystem());
-        
-        PoolManager poolManager = PoolManager.Instance();
-        poolManager.Initialize();
-
-        objectPool = new List<PoolableObject>();
-
-        
     }
 
     // Update is called once per frame
@@ -42,22 +32,6 @@ public class GameManager : MonoBehaviour
         foreach (var up in UpdateList)
         {
             up.SystemUpdate();
-        }
-        
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            PoolManager poolManager = PoolManager.Instance();
-            foreach (PoolableObject obj in objectPool)
-            {
-                poolManager.ReleaseObjectInPool(obj);
-            }
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            PoolManager poolManager = PoolManager.Instance();
-            PoolableObject obj = poolManager.GetObjectInPool(ObjectType.Bullet);
-            objectPool.Add(obj);
         }
     }
 }
